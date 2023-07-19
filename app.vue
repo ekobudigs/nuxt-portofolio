6a4f93b6ecdc6a6b69bd45ab2cfe4c27c533eddd
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-07-19 21:36:14 +0700

    update style

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
    <SiteHeader/>
-  <main>
+  <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
     <NuxtPage /> 
+
   </main>
+  <SiteFooter />
   </div>
 </template>
