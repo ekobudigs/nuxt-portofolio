375bf1d083ba17fbd2c80d27ff33c1c81836f61a
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-07-19 19:12:33 +0700

    create page index

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">👋🏻 Hello, I'm Eko Budi!</h1>
+    <p class="text-base text-gray-900 p-2 italic">Fullstack Developer</p>
+    </section>
+</template>
