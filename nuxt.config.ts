2f810819ab4d76bccd9ce3be28cdda3ad6677935
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-07-21 19:42:49 +0700

    add setting github token and apollo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,25 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
-    '@nuxt/content'
+    '@nuxt/content',
+    '@nuxtjs/apollo'
   ],
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_TOKEN
+  },
   content: {
     // https://content.nuxtjs.org/api/configuration
     highlight: {
       theme: 'nord',
       preload: ['ts', 'js', 'css', 'java', 'json', 'bash', 'vue']
     }
-  }
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: 'github-token',
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
+    },
+  },
 })
