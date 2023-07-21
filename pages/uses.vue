ae6071afbc69059c2ce3935d134bcd7fc907b32f
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-07-21 20:39:44 +0700

    create page uses

---
+++ pages/uses.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1 class="text-3xl font-bold my-8">Uses</h1>
+    <p class="text-lg">
+        Welcome to my personal portfolio website! I enthusiastically want to present the project that I made by following the tutorial on Youtube. With a passion for continuous learning and improvement, I choose the leading and most innovative technologies in the web development industry. This web portfolio is also supported by Nuxt 3, a Vue.js framework that offers great benefits in web application development. The Universal approach (server-side rendering) presented by Nuxt 3 allows this site to have fast load times and is more search engine friendly. Plus, Nuxt 3's easy configuration and powerful features make it easier for me to manage pages, routes, and app state more efficiently. Last, but not least, I leverage the power of GraphQL to manage data capture efficiently and flexibly. Using GraphQL, I can query data with the precision required for a given page, avoid overfetching, and improve overall application efficiency. Thank you for taking the time to visit my portfolio website. I am very excited to share this experience with you and look forward to contributing to more interesting and useful future projects. Feel free to contact me  <a href="https://www.linkedin.com/in/ekobuds/" target="_blank" class="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Eko Budi</a> if you are interested in collaborating or would like to further discuss the project. Enjoy exploring my web portfolio
+       
+    </p>
+  
+    <h2 class="text-2xl font-bold mt-8 mb-2">This Website</h2>
+    <ul class="list-disc">
+      <li class="ml-8 py-1"><a href="https://vuejs.org/">Vue 3</a></li>
+      <li class="ml-8 py-1"><a href="https://nuxt.com/">Nuxt 3</a></li>
+      <li class="ml-8 py-1"><a href="https://tailwindcss.com/">Tailwind CSS</a></li>
+      <li class="ml-8 py-1">Hosted on <a href="https://netlify.com/">Netlify</a></li>
+      <li class="ml-8 py-1"><a href="https://graphql.org/">GraphQL</a></li>
+    </ul>
+  
+    <h2 class="text-2xl font-bold mt-8 mb-2">Editor and Terminal</h2>
+    <ul class="list-disc">
+      <li class="ml-8 py-1"><a href="https://code.visualstudio.com/" aria-label="Visual Studio Code">Visual Studio Code</a>
+       
+      </li>
+      <li class="ml-8 py-1">
+        The theme I am currently using is
+        <a href="https://marketplace.visualstudio.com/items?itemName=Parsinta.parsinta-exclusive"
+         >parsinta-exclusive</a> By Parsinta.
+      </li>
+      <li class="ml-8 py-1"><a href="https://git-scm.com/downloads" aria-label="iTerm">git-scm</a> is my
+        Terminal
+      </li>
+    </ul>
+  
+    <img src="~/assets/images/vscode.png" alt="Visual Studio Code Synthwave 84 Theme" class="mt-8 mb-20" />
+  </template>
